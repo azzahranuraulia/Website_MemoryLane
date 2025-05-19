c61b76bbd875113b5485c4e2bfcae6948a9974ba
azzahra271207 <azzahranurauliaaa27@gmail.com>
2025-05-19 09:07:08 +0700

    railway

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "memorylane";
+$host = "mysql.railway.internal";  // dari Railway
+$user = "root";               // dari Railway
+$pass = "msrQjaUzkBsxElUhzSyNchcLondgZJJV";            // dari Railway
+$db = "railway";              // dari Railway
+$port = 3306;                        // biasanya 3306, cek di Railway
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
+$koneksi = mysqli_connect($host, $user, $pass, $db, $port);
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
